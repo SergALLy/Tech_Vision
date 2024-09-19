2b58646bbd036ef3466f24e93a9a5e3c70950604
SergALLy <cfyz70903@mail.ru>
2024-09-19 21:03:40 +0300

    Made by me

--- Laba_1/task_2.py
+++ Laba_1/task_2.py
@@ -5,7 +5,7 @@ window_name = 'figure 2'
 font = cv2.FONT_HERSHEY_COMPLEX
 fontScale = 0.8
 img = np.full((500,500,3),255,dtype='uint8') # Создаём белый холст
-cv2.line(img,pt1=(500,0),pt2=(0,500),color=(255,0,0), thickness=3) # Рисуем синию линию
+cv2.line(img,pt1=(500,0),pt2=(0,500),color=(255,191,0), thickness=3) # Рисуем синию линию
 cv2.putText(img,'Line',(50,380),font,fontScale,color=(0,0,0))
 cv2.rectangle(img,pt1=(100,100),pt2=(200,150), color=(255,0,139),thickness=3) # Рисуем фиолетовый прямоугольник
 cv2.putText(img,'Restangle',(90,90),font,fontScale,color=(0,0,0))
