62444ac8498119792b7409392245acdc2661380c
SergALLy <cfyz70903@mail.ru>
2024-10-20 23:37:44 +0300

    Laba 4

--- Laba_4/task_1.py
+++ Laba_4/task_1.py
@@ -2,21 +2,20 @@ import cv2, sys
 import numpy as np
 
 def my_morph_open(img,filter,n=1): # Размыкание
-    img_new = cv2.erode(img, filter,iterations=n)
-    img_new = cv2.dilate(img_new, filter, iterations=n)
+    img_new = cv2.erode(img, filter,iterations=n) # Эрозия
+    img_new = cv2.dilate(img_new, filter, iterations=n) # Наращивание
     return img_new
 
 def my_morph_close(img,filter,n): # Замыкание
-    img_new = cv2.dilate(img, filter, iterations=n)
-    img_new = cv2.erode(img_new, filter,iterations=n)
+    img_new = cv2.dilate(img, filter, iterations=n) # Наращивание
+    img_new = cv2.erode(img_new, filter,iterations=n) # Эрозия
     return img_new
 
-def save_or_break(img):
-    name='Photo\\4-1.png'
+def save_or_break(img,number): # Сохранение
+    name=['Photo\\4-1_1.png', 'Photo\\4-1_2.png' ]
     key=cv2.waitKey(0)
     if key == 121: # Нажали Y - сохранить
-        cv2.imwrite(name,img) 
-        sys.exit(1)
+        cv2.imwrite(name[number],img) 
     elif key==27: # Нажали ESC - выйти
         cv2.destroyAllWindows()
         sys.exit(1)
@@ -27,19 +26,40 @@ img = cv2.imread(name, cv2.IMREAD_COLOR)
 cv2.namedWindow(window, cv2.WINDOW_GUI_EXPANDED)
 height= img.shape[0]
 
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+fontScale = 1
+thick=1
+color=(0,0,255)
+
 ksize = np.array([[0.5, 0.75, 0.5],
                   [0.75, 1.0, 0.75],
                   [0.5, 0.75, 0.5]])
+
 while 1:
-    img_new_1 = cv2.morphologyEx(img, cv2.MORPH_OPEN, ksize, iterations=2)
-    img_new_1 = cv2.morphologyEx(img_new_1, cv2.MORPH_CLOSE, ksize, iterations=6)
-    img_new_2 = my_morph_open(img, ksize, 2)
-    img_new_2 = my_morph_close(img_new_2, ksize, 6)
+    img_new_1 = cv2.morphologyEx(img, cv2.MORPH_OPEN, ksize, iterations=2) # Размыкание
+
+    img_new_2 = my_morph_open(img, ksize, 2) # Моё размыкание
+
+    print(np.array_equal(img_new_1, img_new_2)) # Сравниваем изображения
+
+    cv2.putText(img_new_1,'origin_function',(20,40),font,fontScale,color,thick)
+    cv2.putText(img_new_2,'my_function',(20,40),font,fontScale,color,thick)
+    img_2 = np.full((height, 10, 3),(0,0,255),dtype='uint8')
+
+    img_2 = np.hstack((img_new_1, img_2, img_new_2)) # Объединение по горизонтали
+    cv2.imshow(window,img_2)
+    save_or_break(img_2,0)
+
+    img_new_1 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, ksize, iterations=2) # Замыкание
+
+    img_new_2 = my_morph_close(img, ksize, 2) # Моё замыкание
 
     print(np.array_equal(img_new_1, img_new_2))
 
+    cv2.putText(img_new_1,'origin_function',(20,40),font,fontScale,color,thick)
+    cv2.putText(img_new_2,'my_function',(20,40),font,fontScale,color,thick)
     img_2 = np.full((height, 10, 3),(0,0,255),dtype='uint8')
 
     img_2 = np.hstack((img_new_1, img_2, img_new_2))
     cv2.imshow(window,img_2)
-    save_or_break(img_2)
+    save_or_break(img_2,1)
--- Laba_4/task_2.py
+++ Laba_4/task_2.py
@@ -1,12 +1,11 @@
 import cv2, sys
 import numpy as np
 
-def save_or_break(img):
-    name='Photo\\4-1.png'
+def save_or_break(img): # Выход или сохранить
+    name='Photo\\4-2.png'
     key=cv2.waitKey(0)
     if key == 121: # Нажали Y - сохранить
         cv2.imwrite(name,img) 
-        sys.exit(1)
     elif key==27: # Нажали ESC - выйти
         cv2.destroyAllWindows()
         sys.exit(1)
@@ -20,14 +19,10 @@ img_new = np.copy(img)
 ksize = np.array([[0.5, 0.75, 0.5],
                   [0.75, 1.0, 0.75],
                   [0.5, 0.75, 0.5]])
+h = 3
 
 while 1:
-    img_new = cv2.morphologyEx(img_new, cv2.MORPH_OPEN, ksize, iterations=1)
-    img_new = cv2.erode(img_new, ksize,iterations=1)
-    # img_new = cv2.morphologyEx(img_new, cv2.MORPH_CLOSE, ksize, iterations=1)    
-    img_new = cv2.dilate(img_new, ksize,iterations=1)
-    img_new = cv2.erode(img_new, ksize,iterations=1)
-    img_new = cv2.morphologyEx(img_new, cv2.MORPH_CLOSE, ksize, iterations=1)
-    img_new = cv2.dilate(img_new, ksize,iterations=1)
+    img_new = cv2.morphologyEx(img_new, cv2.MORPH_OPEN, ksize, iterations=1,borderType=cv2.BORDER_CONSTANT,borderValue=(255,255,255)) # Размыкание
+    img_new = cv2.medianBlur(img_new,h) # Медианный фильтр
     cv2.imshow(window, img_new)
     save_or_break(img_new)
