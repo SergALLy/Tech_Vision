d2e1b75e55ae13b5afdfe37862e5aa18848e5205
SergALLy <cfyz70903@mail.ru>
2024-11-10 18:19:17 +0300

    completed 3 task Laba_5

---
+++ Laba_5/task_3.py
@@ -0,0 +1,14 @@
+import cv2, sys
+import numpy as np
+
+cap = cv2.VideoCapture('Laba_5\\video_5.mp4')
+while 1:
+    ret, frame = cap.read()
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    frame_filt = cv2.GaussianBlur(frame,ksize=(15,15),sigmaX=0)
+    frame_filt = cv2.Canny(frame_filt, 7, 21, 7)
+    cv2.imshow('video', frame_filt) 
+    if cv2.waitKey(1) == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
